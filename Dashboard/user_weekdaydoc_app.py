ec0835a2b923a0068e09a37b46a1ba291971a6a7
lyla-lee <lky6684@naver.com>
2020-11-06 21:17:47 +0900

    11-6

---
+++ Dashboard/user_weekdaydoc_app.py
@@ -0,0 +1,84 @@
+import dash
+import dash_core_components as dcc
+from jupyter_dash import JupyterDash
+import dash_html_components as html
+from dash.dependencies import Input, State, Output
+import plotly.express as px
+import pandas as pd
+import datetime as dt
+from datetime import date, timedelta
+import calendar
+import dash_daq as daq
+from .Dash_fun import apply_layout_with_auth, load_object, save_object
+
+url_base = '/dash/app3/'
+
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+
+# data loading
+data_path = "/mnt/d/yerachoi/plink-flask-gentelella/data/user_docs_df2.csv"
+df = pd.read_csv(data_path)
+
+# clip_at 컬럼 시간 분 초 삭제
+df.clip_at = df.clip_at.astype(str)
+df['clip_at'] = df['clip_at'].str[:10]
+
+# 요일 생성하기 위해 datetime으로 변환
+df['clip_at'] = pd.to_datetime(df['clip_at'], format = '%Y-%m-%d')
+
+
+# 최근 한 주만 선택한 dataframe
+def select_OneWeek(data):
+  today = date.today()
+  today = pd.to_datetime(today)
+
+  week_prior =  today - timedelta(weeks=1)
+
+  last_week_df = data[week_prior <= data['clip_at']]
+  return last_week_df
+
+# dataframe을 요일별화 시킴
+def weekday_df(data):
+
+  # 요일 칼럼 생성
+  weekday = [ 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+  data['day_of_week'] = data['clip_at'].apply(lambda x: x.weekday()) # get the weekday index, between 0 and 6
+  data['day_of_week'] = data['day_of_week'].apply(lambda x: calendar.day_name[x]) # Monday, Tuesday...
+
+  # Weekday로 groupby한 datarame
+  week_df = data.groupby('day_of_week').count().reset_index() 
+  week_df['day_of_week'] = pd.Categorical(week_df['day_of_week'],categories=weekday, ordered=True)  # Weekday 순서맞추기, not by alphabet order
+  week_df = week_df.sort_values('day_of_week')
+
+  return week_df
+
+
+# 전체 요일별 총 doc 수 그래프
+fig_total = px.bar(weekday_df(df), x='day_of_week', y= 'title', barmode='group')
+fig_total.update_xaxes( title_text='Week Day')
+fig_total.update_yaxes(title_text = 'Total numb of docs')
+
+# 최근 한 주 요일별 총 doc 수 그래프
+fig_oneweek = px.bar(weekday_df(select_OneWeek(df)), x='day_of_week', y= 'title', barmode='group')
+fig_oneweek.update_xaxes( title_text='Week Day')
+fig_oneweek.update_yaxes(title_text = 'Total numb of docs')
+
+layout = html.Div([
+  html.Div([
+      html.Div([
+          html.H3('Total Numb of Doc per Weekday(Total)'),
+          dcc.Graph(id='g1', figure = fig_total)
+      ], className="six columns"),
+
+      html.Div([
+          html.H3('Total Numb of Doc per Weekday(last one week)'),
+          dcc.Graph(id='g2', figure= fig_oneweek)
+      ], className="six columns"),
+  ], className="row")
+])
+
+def Add_Dash(server):
+    app = Dash(server=server, url_base_pathname=url_base)
+    apply_layout_with_auth(app, layout)
+
+    return app.server
--- app/dash/routes.py
+++ app/dash/routes.py
@@ -6,9 +6,19 @@ from Dashboard import Dash_App1, Dash_App2
 @blueprint.route('/app1')
 @login_required
 def app1_template():
-    return render_template('app1.html', dash_url = Dash_App1.url_base)
+    return render_template('app1.html', dash_url = user_timeseries_app.url_base)
 
 @blueprint.route('/app2')
 @login_required
 def app2_template():
-    return render_template('app2.html', dash_url = Dash_App2.url_base)
+    return render_template('app2.html', dash_url = user_totaldoc_app.url_base)
+
+@blueprint.route('/app3')
+@login_required
+def app2_template():
+    return render_template('app3.html', dash_url = user_weekdaydoc_app.url_base)
+
+@blueprint.route('/app4')
+@login_required
+def app2_template():
+    return render_template('app4.html', dash_url = user_clustermap_app.url_base)
