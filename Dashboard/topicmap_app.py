f3cf8ca0c30e2adaf2f6426bf1082abfd0ce3322
uoneway <uoneway@gmail.com>
2020-11-10 03:30:26 +0900

    temp

--- Dashboard/topicmap_app.py
+++ Dashboard/topicmap_app.py
@@ -66,25 +66,27 @@ else:
 # docs 데이터 + top2vec모델 데이터 결합하여 사용할 df 만들기
 topics_idx_vector, docs_idx_vector, words_idx_vector = tm_model.get_2d_vectors()  # id, x, y 리턴
 
-topics_idx_vector_df = pd.DataFrame(topics_idx_vector)
+topic_df = pd.DataFrame(topics_idx_vector)
+topic_df['topic_idx'] = topic_df['id']
 docs_idx_vector_df = pd.DataFrame(docs_idx_vector)
-words_idx_vector_df = pd.DataFrame(words_idx_vector)
+word_df = pd.DataFrame(words_idx_vector)
+word_df['topic_idx'] = -1
 
-topics_idx_vector_df['element_type'] ='topic'
-topics_idx_vector_df['topic_idx'] = topics_idx_vector_df['id']
-docs_idx_vector_df['element_type'] = 'doc'
-words_idx_vector_df['element_type'] = 'word'
-words_idx_vector_df['topic_idx'] = -1
+# topics_idx_vector_df['element_type'] ='topic'
+# topics_idx_vector_df['topic_idx'] = topics_idx_vector_df['id']
+# docs_idx_vector_df['element_type'] = 'doc'
+# words_idx_vector_df['element_type'] = 'word'
+# words_idx_vector_df['topic_idx'] = -1
 
-network_df = pd.concat([topics_idx_vector_df,docs_idx_vector_df, words_idx_vector_df])
-user_docs_df['element_type'] = 'doc'
-network_df = pd.merge(network_df, user_docs_df, on=['id','element_type'], how = 'left')
+#network_df = pd.concat([topics_idx_vector_df,docs_idx_vector_df, words_idx_vector_df])
+#user_docs_df['element_type'] = 'doc'
+doc_df = pd.merge(docs_idx_vector_df, user_docs_df, on=['id'], how = 'left')
 #print(network_df.head())
-print(network_df.isnull().sum())
-print(network_df.columns)
-print(network_df['element_type'].value_counts())
-print(network_df.loc[network_df['element_type'] == 'doc', :][:2])
-print(network_df["topic_idx"].unique())
+# print(network_df.isnull().sum())
+# print(network_df.columns)
+# print(network_df['element_type'].value_counts())
+# print(network_df.loc[network_df['element_type'] == 'doc', :][:2])
+# print(network_df["topic_idx"].unique())
 
 
 ############### network_df에서 'element_type'가 doc인것에 대해  ['title', 'publish_date', 'text_sum', 'url'] 추가해주세요#########
@@ -94,17 +96,17 @@ print(network_df["topic_idx"].unique())
 
 #doc_info_colnames = ['title', 'publish_date', 'contents', 'url', 'crawl_at', 'is_news', 'clip_at', 'contents_prep', 'text_sum', ]  #  'cited_by', 'n_cites', 
 vector_colnames = ['x','y'] # id, topic_idx, word
-position_arr = network_df.loc[:, vector_colnames].to_numpy()
+#position_arr = network_df.loc[:, vector_colnames].to_numpy()
 
-node_size_dict = {"topic": 50, 
-                    "doc": 20, 
-                    "word": 10}
+# position_arr = {"topic": 50, 
+#                     "doc": 20, 
+#                     "word": 10}
 
 # Prep data / fill NAs
 # for colname in doc_info_colnames:
 #     network_df[colname] = network_df[colname].fillna("")
 #network_df["cited_by"] = network_df["cited_by"].fillna("")   # edge하려면 이거 수정해줘야함
-network_df["topic_idx"] = network_df["topic_idx"].astype(str)
+##network_df["topic_idx"] = network_df["topic_idx"].astype(str)
 # topic_idxs = [str(i) for i in range(len(network_df["topic_idx"].unique()))]
 # lda_val_arr = network_df[topic_idxs].values
 
@@ -116,27 +118,55 @@ topics_txt = [[j.split("*")[1].replace('"', "") for j in i] for i in topics_txt]
 
 topics_txt = ["; ".join(i) for i in topics_txt]
 print(topics_txt)
-element_type_ser = network_df.groupby("element_type")["topic_idx"].count().sort_values(ascending=False)
-
+#element_type_ser = network_df.groupby("element_type")["topic_idx"].count().sort_values(ascending=False)
+element_type_ser = {"topic": len(topic_df), 
+                    "doc": len(doc_df),
+                    "word": len(word_df),}
 
 def tsne_to_cyto(tsne_val, scale_factor=40):
     return int(scale_factor * (float(tsne_val)))
 
 
-def get_node_list(in_df):  # Convert DF data to node list for cytoscape
-    return [
-        {
+def get_node_list(topic_df, doc_df, word_df):  # Convert DF data to node list for cytoscape
+    node_list = []
+    # topic
+    node_list += [{
             "data": {
-                "id": str(idx),
-                "origin_id": row["id"],  # 원 db의 아이디. 물론 docs만 의미있...
+                "id": row["id"], #str(idx),
+                "element_type": "topic",
+            },
+            "position": {"x": tsne_to_cyto(row["x"]), "y": tsne_to_cyto(row["y"])},
+            "classes": row["topic_idx"],
+            "selectable": True,
+            "grabbable": False,
+        } for idx, row in topic_df.iterrows()] 
+
+    # doc
+    node_list += [{
+            "data": {
+                "id": row["id"],  # 원 db의 아이디. 물론 docs만 의미있...
                 "title": row["title"],
-                "element_type": row["element_type"],
+                "element_type": "doc",
                 "publish_date": row["publish_date"],
                 "text_sum": row["text_sum"],
                 #"cited_by": row["cited_by"],
                 #"n_cites": row["n_cites"],
                 "node_size": node_size_dict[row["element_type"]], #int(np.sqrt(1 + row["n_cites"]) * 10),
                 "url": row["url"],
+            },
+            "position": {"x": tsne_to_cyto(row["x"]), "y": tsne_to_cyto(row["y"])},
+            "classes": row["topic_idx"],
+            "selectable": True,
+            "grabbable": False,
+        }
+        for idx, row in doc_df.iterrows()
+    ]
+    # word
+    node_list += [
+        {
+            "data": {
+                "id": row["id"],  # 원 db의 아이디. 물론 docs만 의미있...
+                "element_type": "word",
                 "label": row['word']
             },
             "position": {"x": tsne_to_cyto(row["x"]), "y": tsne_to_cyto(row["y"])},
@@ -144,12 +174,15 @@ def get_node_list(in_df):  # Convert DF data to node list for cytoscape
             "selectable": True,
             "grabbable": False,
         }
-        for idx, row in in_df.iterrows()
+        for idx, row in word_df.iterrows()
     ]
+
+    return node_list
+
 
 default_tsne = 40
 
-def update_node_data(dim_red_algo, tsne_perp, in_df, position_arr):
+def update_node_data(dim_red_algo, tsne_perp, topic_df, doc_df, word_df, position_arr):
     #(x_list, y_list) = get_node_locs(vectors_df, dim_red_algo, tsne_perp=tsne_perp)
     x_list = position_arr[:, 0]
     y_list = position_arr[:, 1]
@@ -162,7 +195,7 @@ def update_node_data(dim_red_algo, tsne_perp, in_df, position_arr):
     in_df["x"] = x_list
     in_df["y"] = y_list
 
-    tmp_node_list = get_node_list(in_df)
+    tmp_node_list = get_node_list(topic_df, doc_df, word_df)
     for i in range(
         len(in_df)
     ):  # Re-scaling to ensure proper canvas scaling vs node sizes 
@@ -179,7 +212,7 @@ startup_elm_list = []
 
 col_swatch = cycle(px.colors.qualitative.Plotly) #px.colors.qualitative.Dark24
 def_stylesheet = [ ]
-for topic_idx in network_df["topic_idx"].unique():
+for topic_idx in topic_df["topic_idx"].unique():
     color = next(col_swatch)
     def_stylesheet.append({
         "selector": "." + str(topic_idx),  # word는 -1이기 때문
@@ -291,13 +324,10 @@ body_layout = dbc.Container(
                                     id="element_types_dropdown",
                                     options=[
                                         {
-                                            "label": i
-                                            + " ("
-                                            + str(v)
-                                            + ")",
-                                            "value": i,
+                                            "label": k+ " ("+ str(v)+ ")",
+                                            "value": k,
                                         }
-                                        for i, v in element_type_ser.items()
+                                        for k, v in element_type_ser.items()
                                     ],
                                     value=startup_element_types,
                                     #multi=True,
@@ -377,13 +407,13 @@ def filter_nodes(usr_element_types_list=None):
         #cur_df = network_df[(network_df.n_cites >= usr_min_cites)]
         cur_df = network_df.copy()
         if usr_element_types_list is not None and usr_element_types_list != []:
-            cur_df = cur_df[(cur_df.element_type.isin(usr_element_types_list))]
+            cur_df = cur_df[(cur_df['element_type'].isin(usr_element_types_list))]
 
         position_arr = cur_df.loc[:, vector_colnames].to_numpy()
 
         cur_node_list = update_node_data(dim_red_algo, tsne_perp, in_df=cur_df, position_arr=position_arr)
-        conn_list = []
 
+        conn_list = []
         # if show_edges:
         #     conn_list = draw_edges(cur_df)
 
