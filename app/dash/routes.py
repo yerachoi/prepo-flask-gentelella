0f5cdf65077bfddffadc2dcd8840e8f1a65fd279
Yera Choi <yeracachoi@gmail.com>
2020-11-11 03:19:47 +0900

    edit (temp) for timeseries dash app

--- app/dash/routes.py
+++ app/dash/routes.py
@@ -1,6 +1,6 @@
 from . import blueprint
-from flask import render_template
-from flask_login import login_required
+from flask import render_template, session
+from flask_login import current_user, login_required
 # from Dashboard import Dash_App1, Dash_App2, Dash_App3, Dash_App4
 from Dashboard import user_totaldoc_app, user_weekdaydoc_app 
 from Dashboard import user_timeseries_app, user_clustermap_app
@@ -10,29 +10,15 @@ from Dashboard import hotkeyword_app, topicmap_app
 @blueprint.route('/app1')
 @login_required
 def app1_template():
-<<<<<<< HEAD
-    return render_template('app1.html', dash_url = user_timeseries_app.url_base)
-=======
+    if current_user.is_authenticated:
+        session['username'] = current_user.username
+
     # return render_template('app1.html', dash_url = Dash_App1.url_base)
     return render_template('app1.html', dash_url = user_totaldoc_app.url_base)
->>>>>>> fde1fa00c092367e283bc001679414ecf72a0091
 
 @blueprint.route('/app2')
 @login_required
 def app2_template():
-<<<<<<< HEAD
-    return render_template('app2.html', dash_url = user_totaldoc_app.url_base)
-
-@blueprint.route('/app3')
-@login_required
-def app2_template():
-    return render_template('app3.html', dash_url = user_weekdaydoc_app.url_base)
-
-@blueprint.route('/app4')
-@login_required
-def app2_template():
-    return render_template('app4.html', dash_url = user_clustermap_app.url_base)
-=======
     # return render_template('app2.html', dash_url = Dash_App2.url_base)
     return render_template('app2.html', dash_url = user_weekdaydoc_app.url_base)
 
@@ -59,4 +45,3 @@ def app5_template():
 def app6_template():
     # return render_template('app6.html', dash_url = Dash_App6.url_base)
     return render_template('app6.html', dash_url = topicmap_app.url_base)
->>>>>>> fde1fa00c092367e283bc001679414ecf72a0091
